d11103bc8002a1b54d49e13333b06a7f3544e0cd
Meriana <merianaalchami@gmail.com>
2023-06-07 16:54:33 +0200

    Intégration et décomposition du template en partials

---
+++ app/views/templates/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include_once '../app/views/template/partials/_head.php'; ?>
+
+<body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+    <!-- Header -->
+    <?php include_once '../app/views/template/partials/_header.php'; ?>
+
+    <!-- Main -->
+    <?php include_once '../app/views/template/partials/_main.php'; ?>
+    <!--appel aside et main content dans le main-->
+    <!-- Aside -->
+    <!-- Main content -->
+
+
+    <!-- Footer -->
+    <?php include_once '../app/views/template/partials/_footer.php'; ?>
+</body>
+
+</html>
---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,7 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+    <!-- Aside -->
+    <?php include_once '../app/views/template/partials/_aside.php'; ?>
+
+    <!-- Main content -->
+    <?php include_once '../app/views/template/partials/_mainContent.php'; ?>
+</div>
